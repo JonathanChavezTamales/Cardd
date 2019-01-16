add779ced10149d058538d7ec4be31d04f01829d
Jonathan Chavez <jonathanchatab@hotmail.com>
2019-01-15 18:05:33 -0600

    New card form, minor bugs

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from .models import Card
+from .forms import CardForm
+from django.http import HttpResponse
 # Create your views here.
+
+def all_cards_view(request):
+
+    cards = Card.objects.filter(isPublic=True).order_by('-id')[:10]
+
+
+    return render(request, "cards/home.html", {'cards':cards})
+
+
+
+def new_card_view(request):
+
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+
+        if form.is_valid:
+            card = form.save(commit=False)
+            card.save()
+            return HttpResponse("Guardada")
+
+
+    else:
+        form = CardForm()
+
+    return render(request, 'cards/new_card.html', {'form':form})
