154f722af6e548fb748905d7d27100e920dc6d25
Jonathan Chavez <jonathanchatab@hotmail.com>
2019-01-19 18:40:12 -0600

    hid keys, now are others, you cant steal them

--- cardd/settings.py
+++ cardd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,11 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i0)q-u8a*+o_j_j=rw4#d+a_c+lz_^7(%pi(ltbuuavr3_+zo2'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -145,8 +141,17 @@ LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
 
 
+## PRODUCTION
+
 
 #Keys
 
-SOCIAL_AUTH_TWITTER_KEY = 'KU6ZE7kMU6ZI5DtrksjxIdst0'
-SOCIAL_AUTH_TWITTER_SECRET = 't1WXCfPExw5FApFm4jJuap9MMsCmNX1rII8I77f6H9X9aQRIit'
+SOCIAL_AUTH_TWITTER_KEY = keys.twitter_key
+SOCIAL_AUTH_TWITTER_SECRET = keys.twitter_secret
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = keys.secret
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
