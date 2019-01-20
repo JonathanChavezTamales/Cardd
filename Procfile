65b67d7027960ce6d6857484172085d9177dee14
Jonathan Chavez <jonathanchatab@hotmail.com>
2019-01-19 19:18:56 -0600

    changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bootcamp.wsgi --log-file -
+web: gunicorn cardd.wsgi:application --log-file -
--- cardd/settings.py
+++ cardd/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import keys
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,5 +160,4 @@ DEBUG = False
 
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
